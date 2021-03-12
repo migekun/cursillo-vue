5dd25c86567678e53384c1e68d35c891890ee6fd
Miguel Ángel Navas García <u92040@caib.es>
2021-03-12 16:23:22 +0100

    formulari amb vue

---
+++ formulari/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!--img alt="Vue logo" src="./assets/logo.png"-->
+    <!--HelloWorld msg="Welcome to Your Vue.js App"/-->
+    <Formulari msg="Formulari de proves"/>
+  </div>
+</template>
+
+<script>
+import Formulari from './components/Formulari.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Formulari
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
