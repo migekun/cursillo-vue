f282b732c96f698c2320bf55c02fb066c18e0734
Miguel Ángel Navas García <manavas@gmail.com>
2021-03-16 08:33:54 +0100

    Canvi de la cridada del store

--- formulari/src/main.js
+++ formulari/src/main.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
-import {store} from './store/store'
+import store from './store'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
-/*new Vue({
-  render: h => h(App),
-}).$mount('#app')*/
 new Vue({
-  el: '#app',
   store,
-  components: { App },
-  template: '<App/>'
-});
+  render: h => h(App),
+}).$mount('#app')
 
