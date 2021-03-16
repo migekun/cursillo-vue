aebe2069a852f9e362aebc64da50f2b30a316bf0
Miguel Ángel Navas García <manavas@gmail.com>
2021-03-16 18:32:57 +0100

    modificació per problemes amb el lang scss

--- vueexemplespa/src/components/Login.vue
+++ vueexemplespa/src/components/Login.vue
@@ -15,6 +15,6 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 div {margin:10px}
 </style>
--- vueexemplespa/src/components/Register.vue
+++ vueexemplespa/src/components/Register.vue
@@ -31,7 +31,7 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style  scoped>
 div {
   margin: 10px;
 }
