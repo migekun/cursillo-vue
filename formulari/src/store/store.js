5a6a8bd34e99497dff630d31ff1db592c007ab57
Miguel Ángel Navas García <manavas@gmail.com>
2021-03-16 08:32:41 +0100

    Canvi al store, es posarà la llista de persones

--- formulari/src/store/store.js
+++ formulari/src/store/store.js
@@ -6,30 +6,19 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
   state: {
-    persona: {
-      nom: "",
-      llin1: "",
-      llin2: "",
-      dni: ""
-    },
     persones:[]
   },
   mutations: {
     novaPersona(state, persona){
       state.persones.push(persona);
-      persona = {
-        nom: "",
-        llin1: "",
-        llin2: "",
-        dni: ""
-      }  
     },
     eliminaPersona(state, persona) {
       state.persones.splice(state.persones.indexOf(persona), 1);
     },
   },
   getters: {
-      persona: state => state.persona,
-      persones: state => state.persones
+    getPersones: state => {
+      return state.persones;
+    }
   }
 });
