08e7bb75ba2adceda3319ea211303a9d64c9b54b
Miguel Ángel Navas García <manavas@gmail.com>
2021-03-16 08:57:24 +0100

    Update index.js

--- exemplestore/src/store/index.js
+++ exemplestore/src/store/index.js
@@ -1,24 +1,23 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    llista:[]
-  },
-  getters: {
-    getLlista: state => {
-      return state.llista;
-    }
+    persones: []
   },
   mutations: {
-    afegeixElement:function(state, element){
-      state.llista.push(element);
+    novaPersona(state, persona) {
+      state.persones.push(persona);
     },
-    eliminaElement:function(state, element){
-      let posicio = state.llista.indexOf(element);
-      state.llista.splice(posicio, 1);
+    eliminaPersona(state, persona) {
+      state.persones.splice(state.persones.indexOf(persona), 1);
+    }
+  },
+  getters: {
+    getPersones: (state) => {
+      return state.persones;
     }
   }
-})
+});
